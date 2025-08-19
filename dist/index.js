9b16ac4e87d20b8f6a15b7c942299c32e18a6cdc
Daniel C. Ribeiro <dcoutinho.96@gmail.com>
2025-08-19 20:28:47 -0300

    feat: hello world from graphql :)

--- dist/index.js
+++ dist/index.js
@@ -3,17 +3,11 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "@dcoutinho96/spotify-clone-luizalabs-coding-challenge-graphql-schema";
 const resolvers = {
     Query: {
-        hello: () => "Hello from API with TS!",
+        hello: () => "Hello from GraphQL schema 🎶",
     },
 };
-async function bootstrap() {
-    const server = new ApolloServer({
-        typeDefs,
-        resolvers,
-    });
-    const { url } = await startStandaloneServer(server, {
-        listen: { port: process.env.PORT ? parseInt(process.env.PORT) : 4000 },
-    });
-    console.log(`🚀 API ready at ${url}`);
-}
-bootstrap();
+const server = new ApolloServer({ typeDefs, resolvers });
+const { url } = await startStandaloneServer(server, {
+    listen: { port: process.env.PORT ? Number(process.env.PORT) : 4000 },
+});
+console.log(`🚀 GraphQL ready at ${url}`);
