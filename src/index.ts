271a7e21224f688c9dce9f522e667f30e8410170
Daniel C. Ribeiro <dcoutinho.96@gmail.com>
2025-08-19 20:10:17 -0300

    feat: initial commit with graphql

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "@dcoutinho96/spotify-clone-luizalabs-coding-challenge-graphql-schema";
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello from API with TS!",
+  },
+};
+
+async function bootstrap() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: process.env.PORT ? parseInt(process.env.PORT) : 4000 },
+  });
+
+  console.log(`🚀 API ready at ${url}`);
+}
+
+bootstrap();
